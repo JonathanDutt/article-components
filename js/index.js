15a14822424940a268a3ff8a61d8aa2f68de1510
graphic <graphic@WFD228-36m.local>
2020-02-20 11:16:15 -0500

    Updated after class

--- js/index.js
+++ js/index.js
@@ -1,28 +1,81 @@
-// DOCUMENT REFERENCES
 let $doc = document.documentElement
-let $win = window
+let $lnk = document.querySelector('.slide-from')
 let $bar = document.querySelector('.bar')
-let $prog = document.querySelector('.progress')
-let $h1 = document.querySelector('#heading')
-let $h1copy = document.querySelector('.h1-copy')
+
+// how tall is the document?
+$doc.scrollHeight
+
+// how tall is the window?
+$doc.clientHeight
+
+// capture some key window events that might be involved
+window.addEventListener('scroll', event => { console.log('scrolled') })
+window.addEventListener('resize', event => { console.log('resized') })
+window.addEventListener('hashchange', event => { console.log('hash change') })
+
+//    how much have you scrolled?
+window.scrollY
+
+// capture when a link is clicked
+$lnk.addEventListener('click', event => { console.log('clicked') })
+
+//    where is the anchor pointing?
+$lnk.getAttribute('href')
+
+// which item am i trying to slide to? 
+let $goto = document.querySelector('#document')
+
+// does that element/section exist?
+if ($goto) {
+  // The element exists
+} else {
+  // The element does not exist
+}
+
+// how far is this element from the top of the document?
+$goto.offsetTop
+
+// how far is this element from the top of the viewport?
+$goto.getBoundingClientRect().top
+
+// scroll to an element
+$goto.scrollIntoView()
+$goto.scrollIntoView({ block:'start', behavior:'smooth' })
+
+// scroll to a position
+window.scrollTo(0, 0);
+window.scrollTo({ top:0, left:0, behavior:'smooth' });
+
+// are there any obstructions, if so, what size are they?
+$bar.getBoundingClientRect().height
 
 
-// STEPS
-// FIRST: Test all of the selectors above
+// use the browser's animation framerate to do something over and over again
+let count = 0;  // optional counter
+const loop = () => {
 
-// Get the textContent from the <h1>
+  // Check a condition (here, a max number of times it should run: 10)
+  // If the limit was hit, escape immediately
+  if (++count > 10) return   // "return" quits the function
 
-// Put some textContent into the .h1-copy
+  // Do whatever the actions is
+  console.log(`Iteration number: ${count}`)
 
-// Apply opacity 0 to the .h1-copy
+  // Loop again if we got this far!
+  window.requestAnimationFrame(loop)
+}
+window.requestAnimationFrame(loop) // The first loop!
 
-// Apply opacity 1 to the .h1-copy
 
-// A) Determine the scrollY of the window when the element is scrolled out of view
-// B) Determine how this would be done by looking at the "Properties of the h1"
 
-// if (heading is "in view") by checking how much has scrolled
+// Select all matching elements
+let navAnchors = document.querySelectorAll('nav a')
+// Iterate over each found element once, referring to each one as $a
+navAnchors.forEach($a => { 
 
-// else (heading is NOT "in view") by checking how much has scrolled
+  $a.addEventListener('click', event => { console.log($a.getAttribute('href')) }) 
 
-// Capture the 'scroll' of the window
+})
+
+
+// scrollTo
