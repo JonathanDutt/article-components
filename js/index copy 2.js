b3f9762c2bd841d4b37bce0c07fb1875ab534361
JonathanDutt <Jonathan.dutt@georgebrown.ca>
2020-02-21 09:31:21 -0500

    jscopies

---
+++ js/index copy 2.js
@@ -0,0 +1,29 @@
+// DOCUMENT REFERENCES
+let $doc = document.documentElement
+let $win = window
+let $bar = document.querySelector('.bar')
+let $prog = document.querySelector('.progress')
+let $h1 = document.querySelector('#heading')
+let $h1copy = document.querySelector('.h1-copy')
+
+
+// STEPS
+// FIRST: Test all of the selectors above
+
+// Get the textContent from the <h1>
+
+// Put some textContent into the .h1-copy
+
+// Apply opacity 0 to the .h1-copy
+
+// Apply opacity 1 to the .h1-copy
+
+// A) Determine the scrollY of the window when the element is scrolled out of view
+// B) Determine how this would be done by looking at the "Properties of the h1"
+
+// if (heading is "in view") by checking how much has scrolled
+
+// else (heading is NOT "in view") by checking how much has scrolled
+
+// Capture the 'scroll' of the window
+
--- js/index.js
+++ js/index.js
@@ -1,58 +1,49 @@
 
+
+
 // - Can I select an element by id?
-let $section = document.querySelector('#javascript')
-​
+let $goto = document.querySelector('#javascript')
+
 // - Can I confirm an element exists?
-if ($section) {
+if ($goto) {
   // Yes, an element was found
   console.log('found')
 } else {
   // No, an element was NOT found
-  console.log('not found')
+  console.log('was NOT found')
 }
-​
 // - How far from the top of the document is an element?
-$section.offsetTop
-​
+$goto.offsetTop
 // - How far from the top of the viewport is an element?
-$section.getBoundingClientRect().top
-​
+$goto.getBoundingClientRect().top
 // - How tall is the document?
 document.documentElement.scrollHeight
-​
+document.querySelector('html')
 // - How tall is the window?
 document.documentElement.clientHeight
-​
+
 // - How much has the window currently been scrolled?
 window.scrollY
-​
+
 // - Can I scroll to a particular element?
-$section.scrollIntoView({ behavior:'smooth' })
-​
+$goto.scrollIntoView({ behavior:'smooth' })
+
 // - Can I scroll to a particular px position?
 window.scrollTo({left:0, top:0, behavior:'smooth'})
-​
+
 // - What is the height(s) of any obstruction(s)?
-$section.getBoundingClientRect().height
-​
+$goto.getBoundingClientRect().height
+
 // - Can I capture when an element has triggered a `click` event?
 let $link = document.querySelector('.scroll-to')
 $link.addEventListener('click', event => { console.log('click') })
-​
+
 // - Where is the anchor pointing?
 $link.getAttribute('href')
-​
-​
+
+
 // - Can I capture window events, like: `scroll`, `resize`, `hashchange`?
 window.addEventListener('scroll', event => { console.log('scroll') })
 window.addEventListener('resize', event => { console.log('resize') })
 window.addEventListener('hashchange', event => { console.log('hashchange') })
 Collapse
-
-
-
-
-
-
-
-
