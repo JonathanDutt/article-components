f28979a69f2654bc06ce6dbb1e11aa0f694794b9
JonathanDutt <Jonathan.dutt@georgebrown.ca>
2020-02-21 09:42:57 -0500

    titlebar

--- js/index copy 2.js
+++ js/index copy 2.js
@@ -4,26 +4,25 @@ let $win = window
 let $bar = document.querySelector('.bar')
 let $prog = document.querySelector('.progress')
 let $h1 = document.querySelector('#heading')
-let $h1copy = document.querySelector('.h1-copy')
+let $title = document.querySelector('.title')
 
-
-// STEPS
-// FIRST: Test all of the selectors above
-
-// Get the textContent from the <h1>
-
-// Put some textContent into the .h1-copy
-
-// Apply opacity 0 to the .h1-copy
-
-// Apply opacity 1 to the .h1-copy
-
-// A) Determine the scrollY of the window when the element is scrolled out of view
-// B) Determine how this would be done by looking at the "Properties of the h1"
-
-// if (heading is "in view") by checking how much has scrolled
-
-// else (heading is NOT "in view") by checking how much has scrolled
 
 // Capture the 'scroll' of the window
+$win.addEventListener('scroll', event => {
+  // Get the textContent from the <h1>
+let titleStr = $h1.textContent
 
+let posH = 60
+
+let winScroll = $win.scrollY
+
+// Apply opacity 0 to the .h1-copy
+// Apply opacity 1 to the .h1-copy
+// if (heading is "in view") by checking how much has scrolled
+if (posH >= winScroll) {
+  $title.style.opacity = 0
+} else {
+  $title.style.opacity = 1
+}
+// else (heading is NOT "in view") by checking how much has scrolled
+})
