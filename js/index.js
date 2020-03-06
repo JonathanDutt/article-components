a512ba78cf9ce79f26082fbf5f0387aa3b3bc35c
JonathanDutt <Jonathan.dutt@georgebrown.ca>
2020-03-06 00:05:08 -0500

    Wordcount try

--- js/index.js
+++ js/index.js
@@ -1,51 +1,5 @@
 
-
-
-// - Can I select an element using a dynamic selector string?
-let $goto = document.querySelector('#javascript')
-
-// - Can I confirm an element exists?
-if ($goto) {
-  // Yes, an element was found
-  console.log('found')
-} else {
-  // No, an element was NOT found
-  console.log('was NOT found')
+function wordCount(str) {
+  return str.split(" ").length;
 }
-// - How far from the top of the document is an element?
-ele.offsetTop
-// - How far from the top of the viewport is an element?
-ele.getBoundingClientRect().top
-// - How tall is the document? 
-document.documentElement.scrollHeight
-document.querySelector('html')
-// - How tall is the window?
-document.documentElement.clientHeight
-
-// - How much has the window currently been scrolled?
-window.scrollY
-
-// - Can I scroll to a particular element?
-ele.scrollIntoView({ behavior:'smooth' })
-
-// - Can I scroll to a particular px position?
-window.scrollTo({left:0, top:0, behavior:'smooth'})
-
-// - What is the height(s) of any obstruction(s)?
-ele.getBoundingClientRect().height
-
-// - Can I capture when an element has triggered a `click` event?
-
-
-// - Where is the anchor pointing?
-
-// Can I apply that same functionality to many elements?
-document.querySelectorAll('#')
-// - Can I capture window events, like: `scroll`, `resize`, `hashchange`?
-window.addEventListener('scroll', event => { console.log('scroll') })
-window.addEventListener('resize', event => { console.log('resize') })
-window.addEventListener('hashchange', event => { console.log('hashchange') })
-
-// Can I animate using the browser's framerate?
-
-let count = 0;
+console.log(wordCount(#main)) 
